3b6ff62d9522e06cf74c201f2644428c76cca179
ritorads <ruben.diaz2017@alu.uct.cl>
2023-06-20 01:24:53 -0400

    base model

---
+++ Genetic_algorithm.py
@@ -0,0 +1,47 @@
+def fitness():  # define calidad del cromosoma
+    return 0
+
+
+def normalizar_vector(vector):
+    return vector
+
+
+def creacion_cromosomas():  # creamos el objeto cromosoma
+    return 0
+
+
+def crear_poblacion():
+    return 0
+
+
+def seleccion_cromosomas():  # % de probabilidad de que sea seleccionado por fitness
+    def probabilidad_acumulada():  # definir p de cada cromosoma
+        return 0
+
+    def ultimo_cromosoma_menor_round():
+        return 0
+
+    def sacar_padres():  # obtenemos los que tengan mejor fitness
+        return 0
+
+    return 0
+
+
+def cruzar_cromosomas():
+    return 0
+
+
+def mutar_cromosomas():
+    return 0
+
+
+def llena_poblacion():
+    return 0
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
